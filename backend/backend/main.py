c82785dc7443bdebcd6f571ee385a65419eba252
JASWANT KARUN S A <24cb023@kpriet.ac.in>
2026-02-07 05:08:45 +0530

    final end to end deployed code

--- backend/backend/main.py
+++ backend/backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
 from backend.core.database import Base, engine
 from backend.routers import risk
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://neuro-risk-three.vercel.app/",
+        "https://neuro-risk-git-main-jaswant-karun-s-as-projects.vercel.app/",
+        "https://neuro-risk-2t9lwbzln-jaswant-karun-s-as-projects.vercel.app/"  # Netlify/Vercel
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
